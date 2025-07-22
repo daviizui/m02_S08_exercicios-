9eadf1c4b77995912f4b59b88eaa2cb4d08862de
Davi Izui <davi.izui@gmail.com>
2025-07-22 10:46:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="app-header">
+        <h1>Energia Solar 365</h1>
+        <p>Economize com o sol. Invista no seu futuro</p>
+      </header>
+    </>
+  );
+}
+
+export default App;
